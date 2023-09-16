9d78dad97d10a8daca0107974c338e5c3e0a0791
Aditya <adityagiri1911@gmail.com>
2023-09-16 22:50:58 +0530

    Prakalp project initiated

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Text, Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+    <Box className="first">
+      <Text>Hello World!</Text>
+    </Box>
+    </>
+  );
+}
+
+export default App;
