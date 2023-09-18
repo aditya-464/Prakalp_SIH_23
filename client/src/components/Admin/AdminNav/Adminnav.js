068a40c553e29182a070d6387704718afc91e9ab
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-18 12:58:34 +0530

    add template admin and user nav component

---
+++ client/src/components/Admin/AdminNav/Adminnav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './adminnav.module.css';
+
+export default function Adminnav() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ client/src/components/User/UserNav/Usernav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './usernav.module.css'
+
+export default function Usernav() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
