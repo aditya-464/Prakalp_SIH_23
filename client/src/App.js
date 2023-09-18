a0900881011634dd3a8790f9a5c694488a53275b
Aditya <adityagiri1911@gmail.com>
2023-09-18 15:10:03 +0530

    Client side fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-// import { Text, Box } from "@chakra-ui/react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Navwelcome from './components/Welcome/Navwelcome/Navwelcome';
+import { Text, Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Router>
-      <Navwelcome/>
-    </Router>
+    <>
+    <Box>
+      <Text className="first">Hello World!</Text>
+    </Box>
+    </>
   );
 }
 
