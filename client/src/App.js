87a14a3d8966ecea58b09e39ef184f9949d1509b
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-18 15:47:22 +0530

    add font style in welcome nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 // import { Text, Box } from "@chakra-ui/react";
 // import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Navwelcome from './components/Welcome/Navwelcome/Navwelcome';
+import Navwelcome from "./components/Welcome/Navwelcome/Navwelcome";
+import Adminnav from "./components/Admin/AdminNav/Adminnav";
+import Usernav from "./components/User/UserNav/Usernav";
 
 function App() {
   return (
-      <Navwelcome/>
+    <>
+      <Navwelcome />
+      {/* <br />
+      <Adminnav />
+      <br />
+      <Usernav/> */}
+    </>
   );
 }
 
--- client/src/components/User/UserNav/Usernav.js
+++ client/src/components/User/UserNav/Usernav.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import styles from './usernav.module.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./usernav.module.css";
+import { Text, Button, Flex, Heading, Box } from "@chakra-ui/react";
 
-export default function Usernav() {
+export default function Navwelcome() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className={styles.navBackground}>
+      <Flex alignItems={"center"} justifyContent={"space-between"} py={15}>
+        <NavLink to="/">
+          <Heading
+            as={"h1"}
+            ml={"65px"}
+            style={{ fontWeight: "500", letterSpacing: "1px" }}
+          >
+            Prakalp
+          </Heading>
+        </NavLink>
+        <Box mr={"65px"}>
+          <Button bg="prakalp.0" px={"24px"} py={"19px"} mr={5} className={styles.loginBtn}>
+            <Text className={styles.btnText}>Login</Text>
+          </Button>
+          <Button px={"25px"} py={"20px"} ml={5} className={styles.signupBtn}>
+            <Text className={styles.btnText}>Signup</Text>
+          </Button>
+        </Box>
+      </Flex>
+    </nav>
+  );
 }
