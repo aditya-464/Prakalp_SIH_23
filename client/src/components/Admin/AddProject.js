3a30fe6e0b22eb0455a7122d5a711d38552bd045
shoaibcodez <shoaibrajamolla@gmail.com>
2023-09-19 23:19:26 +0530

    addProject, viewProject updated

--- client/src/components/Admin/AddProject.js
+++ client/src/components/Admin/AddProject.js
@@ -5,130 +5,131 @@ export const AddProject = () => {
 
     return (
         <>
-            <SimpleGrid columns={2} columnGap={100} rowGap={10}>
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Course
-                        </FormLabel>
-                        <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} size={'lg'}>
-                            <option value='option1'>Option 1</option>
-                            <option value='option2'>Option 2</option>
-                            <option value='option3'>Option 3</option>
-                        </Select>
-                    </FormControl>
-                </GridItem>
+            <Box px={'12rem'}>
+                <SimpleGrid columns={2} columnGap={100} rowGap={10} >
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Course
+                            </FormLabel>
+                            <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'}>
+                                <option value='option1'>Option 1</option>
+                                <option value='option2'>Option 2</option>
+                                <option value='option3'>Option 3</option>
+                            </Select>
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Stream
-                        </FormLabel>
-                        <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} size={'lg'}>
-                            <option value='option1'>Computer Science</option>
-                            <option value='option2'>Electronics and Communication Engineering</option>
-                            <option value='option3'>Information Technology</option>
-                        </Select>
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1} colEnd={'3'} >
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Stream
+                            </FormLabel>
+                            <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'}>
+                                <option value='option1'>Computer Science</option>
+                                <option value='option2'>Electronics and Communication Engineering</option>
+                                <option value='option3'>Information Technology</option>
+                            </Select>
+                        </FormControl>
+                    </GridItem>
 
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Project Type
-                        </FormLabel>
-                        <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} size={'lg'}>
-                            <option value='option1'>Individual Project</option>
-                            <option value='option2'>Group Project</option>
-                        </Select>
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Project Type
+                            </FormLabel>
+                            <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'}>
+                                <option value='option1'>Individual Project</option>
+                                <option value='option2'>Group Project</option>
+                            </Select>
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Year
-                        </FormLabel>
-                        <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} size={'lg'}>
-                            <option value='option1'>1st Year</option>
-                            <option value='option2'>2nd Year</option>
-                            <option value='option3'>3rd Year</option>
-                            <option value='option3'>4th Year</option>
-                        </Select>
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Year
+                            </FormLabel>
+                            <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'}>
+                                <option value='option1'>1st Year</option>
+                                <option value='option2'>2nd Year</option>
+                                <option value='option3'>3rd Year</option>
+                                <option value='option3'>4th Year</option>
+                            </Select>
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Student Name
-                        </FormLabel>
-                        <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} size={'lg'} />
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Student Name
+                            </FormLabel>
+                            <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'} />
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            University Roll
-                        </FormLabel>
-                        <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} size={'lg'} />
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                University Roll
+                            </FormLabel>
+                            <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'} />
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Title
-                        </FormLabel>
-                        <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} size={'lg'} />
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Title
+                            </FormLabel>
+                            <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'} />
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl >
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Link
-                        </FormLabel>
-                        <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} size={'lg'} />
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl >
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Link
+                            </FormLabel>
+                            <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'} />
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl >
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Description
-                        </FormLabel>
-                        <Textarea h={'8rem'} placeholder='Type Description' bg={'gray.200'} variant={'Unstyled'} size={'lg'} resize={'none'}
-                            sx={{
-                                '&::-webkit-scrollbar': {
-                                    display: 'none'
-                                },
-                            }} />
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl >
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Description
+                            </FormLabel>
+                            <Textarea h={'10rem'} placeholder='Type Description' bg={'gray.200'} variant={'Unstyled'} fontSize={'1.25rem'} resize={'none'}
+                                sx={{
+                                    '&::-webkit-scrollbar': {
+                                        display: 'none'
+                                    },
+                                }} />
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl >
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Select file(s)
-                        </FormLabel>
-                        <Input type='file' id="file"
-                            style={{ display: "none" }} />
-                        <label htmlFor='file'>
-                            <Box h={'8rem'} p={'5px 10px'} fontSize={'1.125rem'} style={{ border: "dotted 1px gray" }}  >
-                                Select or drag file
-                            </Box>
-                        </label>
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl >
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Select file(s)
+                            </FormLabel>
+                            <Input type='file' id="file"
+                                style={{ display: "none" }} />
+                            <label htmlFor='file'>
+                                <Box h={'10rem'} p={'5px 10px'} fontSize={'1.25rem'} borderRadius={'0.375rem'} style={{ border: "dotted 1px gray" }}  >
+                                    Select or drag file
+                                </Box>
+                            </label>
+                        </FormControl>
+                    </GridItem>
 
-            </SimpleGrid>
-            <HStack my={'6rem'} justify={'center'}  >
-                <Button py={'2rem'} px={'4rem'} letterSpacing={'2px'} borderRadius={'20px'} colorScheme='whatsapp' fontSize={'1.25rem'} variant={'solid'} background={'prakalp.1'} >PUBLISH</Button>
-            </HStack >
-
+                </SimpleGrid>
+                <HStack my={'5rem'} justify={'center'}  >
+                    <Button py={'2rem'} px={'4rem'} letterSpacing={'2px'} borderRadius={'20px'} colorScheme='whatsapp' fontSize={'1.25rem'} variant={'solid'} background={'prakalp.1'} >PUBLISH</Button>
+                </HStack >
+            </Box>
         </>
     )
 }
--- client/src/components/Admin/ViewProject.js
+++ client/src/components/Admin/ViewProject.js
@@ -1,65 +1,67 @@
 import React from 'react'
-import { FormControl, FormLabel, GridItem, SimpleGrid, Select, Input, HStack, Button } from '@chakra-ui/react';
+import { FormControl, FormLabel, GridItem, SimpleGrid, Select, Input, HStack, Button, Box } from '@chakra-ui/react';
 
 export const ViewProject = () => {
     return (
         <>
-            <SimpleGrid columns={2} columnGap={100} rowGap={10}>
+            <Box px={'12rem'}>
+                <SimpleGrid columns={2} columnGap={100} rowGap={10}>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Title
-                        </FormLabel>
-                        <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} size={'lg'} />
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Title
+                            </FormLabel>
+                            <Input placeholder='Type' bg={'gray.200'} variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'} />
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Course
-                        </FormLabel>
-                        <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} size={'lg'}>
-                            <option value='option1'>Option 1</option>
-                            <option value='option2'>Option 2</option>
-                            <option value='option3'>Option 3</option>
-                        </Select>
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Course
+                            </FormLabel>
+                            <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'}>
+                                <option value='option1'>Option 1</option>
+                                <option value='option2'>Option 2</option>
+                                <option value='option3'>Option 3</option>
+                            </Select>
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Stream
-                        </FormLabel>
-                        <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} size={'lg'}>
-                            <option value='option1'>Computer Science</option>
-                            <option value='option2'>Electronics and Communication Engineering</option>
-                            <option value='option3'>Information Technology</option>
-                        </Select>
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Stream
+                            </FormLabel>
+                            <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'}>
+                                <option value='option1'>Computer Science</option>
+                                <option value='option2'>Electronics and Communication Engineering</option>
+                                <option value='option3'>Information Technology</option>
+                            </Select>
+                        </FormControl>
+                    </GridItem>
 
-                <GridItem colSpan={1}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
-                            Year
-                        </FormLabel>
-                        <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} size={'lg'}>
-                            <option value='option0'>All</option>
-                            <option value='option1'>1st Year</option>
-                            <option value='option2'>2nd Year</option>
-                            <option value='option3'>3rd Year</option>
-                            <option value='option4'>4th Year</option>
-                        </Select>
-                    </FormControl>
-                </GridItem>
+                    <GridItem colSpan={1}>
+                        <FormControl isRequired>
+                            <FormLabel fontSize={'2xl'} fontWeight={'bold'}>
+                                Year
+                            </FormLabel>
+                            <Select bg={'gray.200'} placeholder='Select' variant={'Unstyled'} height={'4rem'} fontSize={'1.25rem'}>
+                                <option value='option0'>All</option>
+                                <option value='option1'>1st Year</option>
+                                <option value='option2'>2nd Year</option>
+                                <option value='option3'>3rd Year</option>
+                                <option value='option4'>4th Year</option>
+                            </Select>
+                        </FormControl>
+                    </GridItem>
 
-            </SimpleGrid>
-            <HStack my={'6rem'} justify={'center'}  >
-                <Button py={'2rem'} px={'4rem'} letterSpacing={'2px'} borderRadius={'20px'} colorScheme='whatsapp' fontSize={'1.25rem'} variant={'solid'} background={'prakalp.1'} >Search</Button>
-            </HStack >
+                </SimpleGrid>
+                <HStack my={'5rem'} justify={'flex-end'}  >
+                    <Button py={'2rem'} px={'4rem'} letterSpacing={'2px'} borderRadius={'20px'} colorScheme='whatsapp' fontSize={'1.25rem'} variant={'solid'} background={'prakalp.1'} >Search</Button>
+                </HStack >
+            </Box>
         </>
     )
 }
