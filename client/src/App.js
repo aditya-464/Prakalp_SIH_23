0902536c43d8d8bd6bc877acdabf325cf49d2356
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-19 13:14:09 +0530

    change nav hover style

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,14 @@ import Navwelcome from "./components/Welcome/Navwelcome/Navwelcome";
 import Adminnav from "./components/Admin/AdminNav/Adminnav";
 import Usernav from "./components/User/UserNav/Usernav";
 import Heroadmin from "./components/Admin/HeroAdmin/Heroadmin";
-import Switchbutton from "./components/Admin/SwitchButton/Switchbutton";
 
 function App() {
   return (
     <>
-      {/* Welcome Navbar */}
-      <Navwelcome />
-      {/* Admin Navbar */}
+      {/* <Navwelcome /> */}
+      {/* <Usernav/> */}
       <Adminnav />
-      {/* <Heroadmin/>
-      <Switchbutton/> */}
-      {/* User Navbar */}
-      <Usernav/>
+      <Heroadmin/>
     </>
   );
 }
