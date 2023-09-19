60957632ac09af4b3f18c4294fcab91f12580747
shoaibcodez <shoaibrajamolla@gmail.com>
2023-09-19 19:59:18 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,7 @@ import { Box } from '@chakra-ui/react';
 function App() {
   return (
     <>
-      <Box className="first">
-        <HomeAdmin />
-      </Box>
+      <HomeAdmin />
       {/* <br />
       <Adminnav />
       <br />
