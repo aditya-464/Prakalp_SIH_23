6d12f58b97a67ebfda395ed3a912c1373f551c11
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-20 22:56:33 +0530

    update folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import Navwelcome from "./components/Welcome/Navwelcome/Navwelcome";
 import Adminnav from "./components/Admin/AdminNav/Adminnav";
 import Usernav from "./components/User/UserNav/Usernav";
-import { HomeAdmin } from "./pages/HomeAdmin";
+import { HomeAdmin } from "./pages/HomeAdmin/HomeAdmin";
 import Heroadmin from "./components/Admin/HeroAdmin/Heroadmin";
 import Herouser from "./components/User/HeroUser/Herouser";
 
--- client/src/pages/HomeAdmin/HomeAdmin.js
+++ client/src/pages/HomeAdmin/HomeAdmin.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import Adminnav from "../components/Admin/AdminNav/Adminnav";
-import Heroadmin from '../components/Admin/HeroAdmin/Heroadmin';
-import { AddProject } from '../components/Admin/AddProject';
+import Adminnav from "../../components/Admin/AdminNav/Adminnav";
+import Heroadmin from '../../components/Admin/HeroAdmin/Heroadmin';
+import { AddProject } from '../../components/Admin/AddProject/AddProject';
 import { Box } from '@chakra-ui/react';
-import { ViewProject } from '../components/Admin/ViewProject';
-import AddViewbtn from '../components/Admin/AddViewBtn/AddViewbtn';
+import { ViewProject } from '../../components/Admin/ViewProject/ViewProject';
+import AddViewbtn from '../../components/Admin/AddViewBtn/AddViewbtn';
 
 export const HomeAdmin = () => {
 
