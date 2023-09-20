a25dce9e22d3b0cba550e32a3d8155301024fbe8
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-21 00:33:16 +0530

    update footer spelling mistake

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <HStack minW={'500px'} py={'20px'} color={'white'} bg={'prakalp.5'} justifyContent={'center'}>
-      <Text className={styles.footerText}>Copyright 2023 | All Rights Reserved | Smart India Hackathon 2023 | Mabe by Emerging_DEVS</Text>
+      <Text className={styles.footerText}>Copyright 2023 | All Rights Reserved | Smart India Hackathon 2023 | Made by Emerging_DEVS</Text>
     </HStack>
   )
 }
