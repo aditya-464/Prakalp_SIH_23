5a9221e1a1f9e7ddcd1d53498971cf96a38716bf
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-20 23:15:08 +0530

    changed nav glow style

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import Navwelcome from "./components/Welcome/Navwelcome/Navwelcome";
 import Adminnav from "./components/Admin/AdminNav/Adminnav";
 import Usernav from "./components/User/UserNav/Usernav";
+import { HomeAdmin } from "./pages/HomeAdmin/HomeAdmin";
 import Heroadmin from "./components/Admin/HeroAdmin/Heroadmin";
 import Herouser from "./components/User/HeroUser/Herouser";
-import Largebtn from "./components/LargeButton/Largebtn";
 
 function App() {
-  const name = "Aditya"
   return (
     <>
+      <HomeAdmin />
+      {/* <br />
+      <Adminnav />
+      <br />
+      <Usernav/> */}
       {/* <Navwelcome /> */}
       {/* <Usernav/> */}
-      <Adminnav />
-
-
-      <Heroadmin/>
+      {/* <Adminnav /> */}
+      {/* <Usernav/>
+      <Herouser/> */}
+      {/* <Heroadmin /> */}
       {/* <Herouser name={name}/> */}
-
-
-      <Largebtn/>
 
 
     </>
