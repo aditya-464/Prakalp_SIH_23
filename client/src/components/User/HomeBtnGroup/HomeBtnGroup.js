521bd869f1f2f7644a5a315c9ae1ec58bb6c7970
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-21 23:51:09 +0530

    add user home page button group component

--- client/src/components/User/HomeBtnGroup/HomeBtnGroup.js
+++ client/src/components/User/HomeBtnGroup/HomeBtnGroup.js
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
 import styles from "./homebtngroup.module.css";
 import { Button, ButtonGroup, Flex, Text } from "@chakra-ui/react";
-import { FaEye, FaUniversity } from "react-icons/fa";
+import { FaUniversity } from "react-icons/fa";
 import { BsFillBookmarkFill, BsFire, BsSearch } from "react-icons/bs";
 
 export default function Largebtn(props) {
-
-  const [button, setButton] = useState("btn1");
-  const handleLeftBtn = () => {
-    setButton("btn1");
+  const [button, setButton] = useState("following");
+  const handleBtn = (e) => {
+    setButton(e.currentTarget.id);
   };
-  const handleRightBtn = () => {
-    setButton("btn2");
-  };
+  const activeBtn = (button) =>{
+    let left = "";
+    switch (button) {
+        case "following":
+            left = "0";
+            break;
+        case "bookmarks":
+            left = "250px";
+            break;
+        case "trending":
+            left = "500px";
+            break;
+        case "search":
+            left = "716px";
+            break;
+        default:
+            left = "0";
+            break;
+    }
+    return left;
+  }
   return (
     <Flex my={"30px"} alignItems={"center"} justifyContent={"center"}>
       <ButtonGroup
@@ -24,61 +41,65 @@ export default function Largebtn(props) {
         <div
           id={"btn"}
           className={styles.btn}
-          style={{ left: button === "btn1" ? "0" : "240px" }}
+          style={{ left: activeBtn(button) }}
         ></div>
-        <Button onClick={() => {
-          handleLeftBtn();
-          props.setShowView(true);
-        }}
+        <Button
+          onClick={(e) => {
+            handleBtn(e);
+            props.setShowView(true);
+          }}
           bg={"prakalp.0"}
           aria-label="Add to friends"
           leftIcon={<FaUniversity size={25} />}
           size={"lg"}
           className={styles.rightBtn}
-          id="btn2"
-          color={button === "btn2" ? "white" : "black"}
+          id="following"
+          color={button === "following" ? "white" : "black"}
         >
           <Text mx={5}>Following</Text>
         </Button>
-        <Button onClick={() => {
-          handleRightBtn();
-          props.setShowView(true);
-        }}
+        <Button
+          onClick={(e) => {
+            handleBtn(e);
+            props.setShowView(true);
+          }}
           bg={"prakalp.0"}
           aria-label="Add to friends"
           leftIcon={<BsFillBookmarkFill size={25} />}
           size={"lg"}
           className={styles.rightBtn}
-          id="btn2"
-          color={button === "btn2" ? "white" : "black"}
+          id="bookmarks"
+          color={button === "bookmarks" ? "white" : "black"}
         >
           <Text mx={5}>Bookmarks</Text>
         </Button>
-        <Button onClick={() => {
-          handleRightBtn();
-          props.setShowView(true);
-        }}
+        <Button
+          onClick={(e) => {
+            handleBtn(e);
+            props.setShowView(true);
+          }}
           bg={"prakalp.0"}
           aria-label="Add to friends"
           leftIcon={<BsFire size={25} />}
           size={"lg"}
           className={styles.rightBtn}
-          id="btn2"
-          color={button === "btn2" ? "white" : "black"}
+          id="trending"
+          color={button === "trending" ? "white" : "black"}
         >
           <Text mx={5}>Trending</Text>
         </Button>
-        <Button onClick={() => {
-          handleRightBtn();
-          props.setShowView(true);
-        }}
+        <Button
+          onClick={(e) => {
+            handleBtn(e);
+            props.setShowView(true);
+          }}
           bg={"prakalp.0"}
           aria-label="Add to friends"
           leftIcon={<BsSearch size={25} />}
           size={"lg"}
           className={styles.rightBtn}
-          id="btn2"
-          color={button === "btn2" ? "white" : "black"}
+          id="search"
+          color={button === "search" ? "white" : "black"}
         >
           <Text mx={5}>Search</Text>
         </Button>
