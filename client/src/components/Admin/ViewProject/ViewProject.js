8ec0b0e9e656fbb0aba3d92642bb0bffbc495ef9
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-21 18:19:00 +0530

    .

--- client/src/components/Admin/ViewProject/ViewProject.js
+++ client/src/components/Admin/ViewProject/ViewProject.js
@@ -74,7 +74,6 @@ export const ViewProject = () => {
             </FormControl>
           </GridItem>
 
-<<<<<<< HEAD
           <GridItem colSpan={1}>
             <FormControl isRequired>
               <FormLabel fontSize={"3xl"} fontWeight={"bold"}>
@@ -116,41 +115,3 @@ export const ViewProject = () => {
     </>
   );
 };
-=======
-                    <GridItem colSpan={1}>
-                        <FormControl isRequired>
-                            <FormLabel fontSize={'3xl'} fontWeight={'bold'}>
-                                Stream
-                            </FormLabel>
-                            <Select bg={'prakalp.2'} placeholder='Select' variant={'Unstyled'} height={'5rem'} fontSize={'1.5rem'}>
-                                <option value='option1'>Computer Science</option>
-                                <option value='option2'>Electronics and Communication Engineering</option>
-                                <option value='option3'>Information Technology</option>
-                            </Select>
-                        </FormControl>
-                    </GridItem>
-
-                    <GridItem colSpan={1}>
-                        <FormControl isRequired>
-                            <FormLabel fontSize={'3xl'} fontWeight={'bold'}>
-                                Year
-                            </FormLabel>
-                            <Select bg={'prakalp.2'} placeholder='Select' variant={'Unstyled'} height={'5rem'} fontSize={'1.5rem'}>
-                                <option value='option0'>All</option>
-                                <option value='option1'>1st Year</option>
-                                <option value='option2'>2nd Year</option>
-                                <option value='option3'>3rd Year</option>
-                                <option value='option4'>4th Year</option>
-                            </Select>
-                        </FormControl>
-                    </GridItem>
-
-                </SimpleGrid>
-                <HStack mt={'5rem'} justify={'flex-end'}  >
-                    <Button py={'2rem'} px={'4rem'} letterSpacing={'2px'} borderRadius={'20px'} colorScheme='whatsapp' fontSize={'1.5rem'} variant={'solid'} background={'prakalp.1'} >SEARCH</Button>
-                </HStack >
-            </Box>
-        </>
-    )
-}
->>>>>>> 61c64afce1c5474b3ec89eb3dca8b0b439562a7f
