68de41c7644ab626e64cc66c14c780827393c900
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-09-21 17:58:59 +0530

    changed add view button issue

--- client/src/components/Admin/AddViewBtn/AddViewbtn.js
+++ client/src/components/Admin/AddViewBtn/AddViewbtn.js
@@ -29,7 +29,7 @@ export default function Largebtn(props) {
         <Button onClick={(e) => {
           e.preventDefault();
           handleLeftBtn();
-          props.setShowView(prev => !prev);
+          props.setShowView(false);
         }}
           bg={"prakalp.0"}
           color={button === "btn1" ? "white" : "black"}
@@ -49,7 +49,7 @@ export default function Largebtn(props) {
         </Button>
         <Button onClick={() => {
           handleRightBtn();
-          props.setShowView(prev => !prev);
+          props.setShowView(true);
         }}
           bg={"prakalp.0"}
           aria-label="Add to friends"
