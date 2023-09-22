bee28d3c128bd62c44e0fdb30797265846e8dd1f
sneha-2510 <ranisnehaaa@gmail.com>
2023-09-22 11:58:38 +0530

    Welcome Page done

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Adminnav from "./components/Admin/AdminNav/Adminnav";
 import Usernav from "./components/User/UserNav/Usernav";
 import Heroadmin from "./components/Admin/HeroAdmin/Heroadmin";
 import Herouser from "./components/User/HeroUser/Herouser";
-import Footer from "./components/Footer/Footer";
 import HeroWelcome from "./components/Welcome/HeroWelcome/HeroWelcome";
 
 function App() {
@@ -11,14 +10,14 @@ function App() {
     <>
       <Navwelcome />
       <HeroWelcome/>
-{/* 
-      <Usernav/>
-      <Herouser/> */}
+
+      {/* <Usernav/> */}
+      {/* <Herouser/>  */}
 
 
       {/* * <Adminnav /> */}
       {/*<Heroadmin/> */}
-      <Footer/>
+      
     </>
   );
 }
--- client/src/components/Welcome/HeroWelcome/HeroWelcome.js
+++ client/src/components/Welcome/HeroWelcome/HeroWelcome.js
@@ -1,13 +1,130 @@
 import React from 'react'
-import {Box} from '@chakra-ui/react'
+import styles from "./herowelcome.module.css";
+import { Box, Text, Flex, Image } from '@chakra-ui/react'
 
-function HeroWelcome() {
+import welcome1 from '../../../assets/images/welcome1.jpg'
+import welcome2 from '../../../assets/images/welcome2.jpg'
+import welcome3 from '../../../assets/images/welcome3.jpg'
+import img from '../../../assets/images/jharkhandLogo.png'
+
+
+export default function HeroWelcome() {
   return (
-   <Box  h='calc(100vh)'
-   >
+    <>
+      {/* header */}
+      <Flex pl={65} pt={8} pb={8}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        className={styles.heroBackground} >
 
-   </Box>
+        <Box width={"33.33%"}>
+          <Text
+            fontSize={"6xl"}
+            color={"white"}
+            className={styles.heroHeadText}
+            pb={8}
+          >
+            Prakalp
+          </Text>
+
+          <Text fontSize={'2xl'} color={'white'} className={styles.heroParaText}>Initiative by the Government of Jharkhand to provide centralized platform for submission of projects across all institutions in India. </Text>
+        </Box>
+
+        <Box mr={85} >
+          <Image src={img} width={"180px"} />
+        </Box>
+
+      </Flex>
+
+      {/* 1st row */}
+      <Flex ml={65} mt={100} mr={65} mb={100}  justifyContent={"space-between"} >
+
+        <Box w={"40%"} h={""}  pl={20}>
+          <Text
+            fontSize={"6xl"}
+            fontWeight={"bold"}
+            color={"Black"}
+            className={styles.heroHeadText}
+            pb={8}
+          >
+            Centralized Platform for Project submission
+          </Text>
+
+          <Text fontSize={'2xl'}
+            className={styles.heroParaText}
+          >
+            Projects can be categorized under various levels and easily stored in pdf format along with proper documentation
+          </Text>
+
+        </Box>
+
+        <Box w={"35%"} mr={65} >
+          <Image src={welcome1} width={"100%"} />
+        </Box>
+
+      </Flex>
+
+      {/* 2nd row */}
+      <Flex m={65} justifyContent={"space-between"}>
+
+      <Box w={"35%"} ml={65} >
+          <Image src={welcome2} width={"100%"} />
+        </Box>
+
+        <Box w={"40%"} pr={20}>
+          <Text
+            fontSize={"6xl"}
+            fontWeight={"bold"}
+            color={"Black"}
+            className={styles.heroHeadText}
+            pb={8}
+            textAlign={"right"}
+          >
+            Multi-level Plagiarism Detection
+          </Text>
+
+          <Text fontSize={'2xl'}
+            className={styles.heroParaText}
+            textAlign={"right"}
+          >
+           For every project, Level 1 plagiarism check is done within the system database and Level 2 check is done throughout the internet.
+          </Text>
+
+        </Box>
+
+       
+
+      </Flex>
+
+      {/* 3rd row */}
+      <Flex ml={65} mt={100} mr={65} mb={100} justifyContent={"space-between"} >
+
+        <Box w={"40%"} pl={20}>
+          <Text
+            fontSize={"6xl"}
+            fontWeight={"bold"}
+            color={"Black"}
+            className={styles.heroHeadText}
+            pb={8}
+          >
+           Personalized Projects Recommendation
+          </Text>
+
+          <Text fontSize={'2xl'}
+            className={styles.heroParaText}
+          >
+            Using Artificial Intelligence(AI), viewers will be suggested projects in their feed section based on their search activity and past history.
+          </Text>
+
+        </Box>
+
+        <Box w={"35%"} mr={65}>
+          <Image src={welcome3} width={"100%"} />
+        </Box>
+
+      </Flex>
+    </>
   )
 }
 
-export default HeroWelcome
+
